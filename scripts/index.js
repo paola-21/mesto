2096bbe8b9fa495b68c40f0e34b54105ba2749d3
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2022-12-19 16:03:41 +0300

    на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,32 @@
-const editButton = document.querySelector('.editButton');
+const editButton = document.querySelector('.edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
 
 let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.popup__input-header');
-let jobInput = formElement.querySelector('.popup__input-text');
+let nameInput = formElement.querySelector('.form__input_text_header');
+let jobInput = formElement.querySelector('.form__input_text_text');
 let profileName = document.querySelector('.profile__header');
 let profileJob = document.querySelector('.profile__text');
 
-editButton.addEventListener ('click', function() {
+function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-})
-
-function Close() {
-  popup.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener ('click', Close)
+function close() {
+  popup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  Close();
+  close();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 
+editButton.addEventListener ('click', openPopup);
+
+closeButton.addEventListener ('click', close);
