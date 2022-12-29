a459f71027ea0fcac4b80fccaa5e86bd0f9d514b
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2022-12-29 22:25:03 +0300

    feature/sixboxs

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,53 @@ formElement.addEventListener('submit', handleFormSubmit);
 editButton.addEventListener ('click', openPopup);
 
 closeButton.addEventListener ('click', close);
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const elementContainer = document.querySelector ('.elements');
+const elementTemplate = document.querySelector('.template').content.querySelector('.element');
+
+function createElement(item) {
+  const element = elementTemplate.cloneNode(true);
+
+  element.querySelector('.element__text').textContent = item.name;
+  element.querySelector('.element__foto').src = item.link;
+  element.querySelector('.element__foto').alt = item.link;
+
+  return element;
+}
+
+function renderElement() {
+  initialCards.forEach(item => {
+    const elementHtml = createElement(item);
+    elementContainer.append(elementHtml);
+  });
+}
+
+renderElement();
