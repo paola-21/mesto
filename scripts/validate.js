1c21ac440df82beada13842306bcf3056f67d72e
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2023-01-22 17:22:59 +0300

    validation

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+const config = ({
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__input-button',
+  inactiveButtonClass: 'form__input-button_inactive',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_active'
+});
