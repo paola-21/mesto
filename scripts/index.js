6288062f8fc29a899420faba36fb789adc251424
Olga Panyushkina <olga-nikolaevna-1995@yandex.ru>
2023-01-23 19:03:56 +0300

    enableValidation

--- scripts/index.js
+++ scripts/index.js
@@ -114,14 +114,12 @@ function renderInitialCards() {
 
 renderInitialCards();
 
-buttonOpenAddCardForm.addEventListener ('click', (inputList, buttonElement) => {
-  openPopup(popupNewCard);
-  //toggleButtonState(inputList, buttonElement);
-  
-
-
-
+buttonOpenAddCardForm.addEventListener ('click', () => {
+  openPopup(popupNewCard);  
+  enableValidation(false);
 });
+
+
 
 //фунция добавления карточки
 function submitCard (evt) {
@@ -139,64 +137,4 @@ formNewCard.addEventListener('submit', submitCard);
 
 
 //валидация формы
-
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(config.inputErrorClass);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(config.errorClass);
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(config.inputErrorClass);
-  errorElement.classList.remove(config.errorClass);
-  errorElement.textContent = '';
-};
-
-const isValid = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  })
-};
-
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add(config.inactiveButtonClass);
-    buttonElement.setAttribute('disabled', 'кнопка отключена');
-  } else {
-    buttonElement.classList.remove(config.inactiveButtonClass);
-    buttonElement.removeAttribute('disabled');
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
-  const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  toggleButtonState(inputList, buttonElement);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      isValid(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(config.formSelector));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    });
-    setEventListeners(formElement);
-  });
-};
-
 enableValidation();
